bf05a27825eb22cca241afa014de0b2764bdefca
bwgjoseph <josephgan@live.com.sg>
2023-10-29 15:51:39 +0800

    add generic conditional validation

--- src/main/java/com/bwgjoseph/springbootgroupvalidation/AddressDto.java
+++ src/main/java/com/bwgjoseph/springbootgroupvalidation/AddressDto.java
@@ -1,5 +1,8 @@
 package com.bwgjoseph.springbootgroupvalidation;
 
-@ValidateForHome(message = "ownership is a must!")
+import jakarta.validation.constraints.Min;
+
+// @ValidateForHome(message = "ownership is a must!")
+@Conditional(selected = "type", values = {"home"}, required = {"ownership"})
 // ownership is only required if type is home
-public record AddressDto(String address, String postalCode, String country, String type, String ownership) {}
+public record AddressDto(String address, String postalCode, @Min(10) String country, String type, String ownership) {}
